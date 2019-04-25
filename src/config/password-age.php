db06a36e9b92f4189593a684b71a184917d5c866
ash-allen <mail@ashallendesign.co.uk>
2019-04-25 19:26:06 +0100

    Updates before initial pre-release push.

--- src/config/password-age.php
+++ src/config/password-age.php
@@ -1,7 +1,32 @@
 <?php
 
 return [
-    'expiry-age' => '30 days',
+    /*
+    |--------------------------------------------------------------------------
+    | Password Expiry Age
+    |--------------------------------------------------------------------------
+    |
+    | The amount of time that a password should be allowed to be used
+    | before an expiry notification is sent to them to prompt the
+    | user to update it.
+    |
+    | The date here is parsed using Carbon, so you have a reasonable
+    | amount of flexibility when defining the time.
+    |
+    */
+    'expiry-age' => '3 months',
 
+    /*
+    |--------------------------------------------------------------------------
+    | Interval Between Sending Notifications
+    |--------------------------------------------------------------------------
+    |
+    | This defines the amount of time that should be taken in between sending
+    | notifications to the user. For example, if the interval is set to
+    | 5 days and a user has already been sent a notification, we
+    | will not send them another expiry notification for at
+    | least another 5 days.
+    |
+    */
     'notification-interval' => '5 days'
 ];
